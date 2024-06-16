ee89b1fbb58a9f8ce9884e97dce3709a22af96f0
haseong23 <151842154+haseong23@users.noreply.github.com>
2024-06-17 00:26:24 +0900

    Title: 알파벳에 따른 평가, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240617/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,14 @@
+txt = input()
+
+if txt == 'S':
+    print('Superior')
+elif txt == 'A':
+    print('Excellent')
+elif txt == 'B':
+    print('Good')
+elif txt == 'C':
+    print('Usually')
+elif txt == 'D':
+    print('Effort')
+else:
+    print('Failure')
