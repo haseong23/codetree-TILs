4becf43137df7b13bf5c36341a194f12a211373b
haseong23 <151842154+haseong23@users.noreply.github.com>
2024-06-17 22:28:09 +0900

    Title: 장학금, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240617/장학금/scholarship.py
+++ 240617/장학금/scholarship.py
@@ -1,11 +1,8 @@
 mid, last = map(int, input().split())
 
-if mid >= 90:
-    if last >= 95:
-        print(100000)
-    elif last >= 90:
-        print(50000)
-    else:
-        print(0)
+if m >= 90 and f >= 95:
+	print("100000")
+elif m >= 90 and f >= 90:
+	print("50000")
 else:
-    print(0)
+	print("0")
