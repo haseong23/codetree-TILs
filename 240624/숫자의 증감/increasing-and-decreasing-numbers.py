af09fd5805ba67b0d07673539c072b50aeea74be
haseong23 <151842154+haseong23@users.noreply.github.com>
2024-06-24 21:48:34 +0900

    Title: 숫자의 증감, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240624/숫자의 증감/increasing-and-decreasing-numbers.py
+++ 240624/숫자의 증감/increasing-and-decreasing-numbers.py
@@ -2,10 +2,8 @@ c, n = input().split(" ")
 n = int(n)
 
 if c == "A":
-    for i in range(n):
-        print(i+1, end=" ")
-        i += 1
+    for i in range(1, n+1):
+        print(i, end=" ")
 elif c == "D":
-    for i in range(n):
-        print(n-i, end=" ")
-        i += 1
+    for i in range(n, 0, -1):
+        print(i, end=" ")
