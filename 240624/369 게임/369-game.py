ba541b916f9aa7be1b21e443508429754ef8227b
haseong23 <151842154+haseong23@users.noreply.github.com>
2024-06-24 22:56:54 +0900

    Title: 369 게임, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240624/369 게임/369-game.py
+++ 240624/369 게임/369-game.py
@@ -3,10 +3,13 @@ n = int(input())
 for i in range(1, n+1):
     i = str(i)
     l = len(i)
+    check = 0
+
     for j in range(l):
         if i[j] == "3" or i[j] == "6" or i[j] == "9":
-            print("0", end=" ")
-            break
-        else:
-            print(i, end=" ")
-            break
+            check += 1
+    
+    if check > 0:
+        print("0")
+    else:
+        print(i, end=" ")
