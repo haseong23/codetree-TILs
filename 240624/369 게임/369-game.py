09f0789ab5461cc889ad5be6d45aa581c473f63d
haseong23 <151842154+haseong23@users.noreply.github.com>
2024-06-24 22:58:54 +0900

    Title: 369 게임, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240624/369 게임/369-game.py
+++ 240624/369 게임/369-game.py
@@ -1,14 +1,17 @@
 n = int(input())
 
 for i in range(1, n+1):
-    i = str(i)
-    l = len(i)
-    check = 0
+    if i % 3 == 0:
+        check += 1
+    else:
+        i = str(i)
+        l = len(i)
+        check = 0
 
-    for j in range(l):
-        if i[j] == "3" or i[j] == "6" or i[j] == "9":
-            check += 1
-    
+        for j in range(l):
+            if i[j] == "3" or i[j] == "6" or i[j] == "9":
+                check += 1
+
     if check > 0:
         print("0", end=" ")
     else:
