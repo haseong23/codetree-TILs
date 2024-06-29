253856c03d3f78c53ffe85b8fb434d1cd0ea7fb2
haseong23 <151842154+haseong23@users.noreply.github.com>
2024-06-29 10:27:27 +0900

    Title: 몫 출력하기, Time: 150ms, Memory: 24MB, Status: Passed - CodeTree

--- 240629/몫 출력하기/print-share.py
+++ 240629/몫 출력하기/print-share.py
@@ -1,9 +1,13 @@
 cnt = 0
 
 while True:
-    if cnt == 3:
-        break
     n = int(input())
-    if n % 2 == 0:
-        print(n // 2)
-        cnt += 1
+
+    if n % 2 == 1:
+        continue
+    
+    print(n // 2)
+    cnt += 1
+    
+    if cnt >= 3:
+        break
