d84c365cb1f7e279583e5da710689d8a151db5c9
haseong23 <151842154+haseong23@users.noreply.github.com>
2024-07-28 02:43:04 +0900

    Title: 1-9 정사각형, Time: 123ms, Memory: 73MB, Status: Passed - CodeTree

--- 240728/1-9 정사각형/1-9-square.py
+++ 240728/1-9 정사각형/1-9-square.py
@@ -2,13 +2,10 @@ n = int(input())
 
 cnt = 1
 
-for i in range(n):
-    for j in range(n):
-        if cnt > 9:
-            cnt %= 10
-        print(cnt, end="")
-        if cnt == 9:
-            cnt += 2
-        else:
-            cnt += 1
-    print()
+for _ in range(n):
+	for _ in range(n):
+		print(cnt, end="")
+		cnt += 1
+		if cnt == 10:
+			cnt = 1
+	print()
