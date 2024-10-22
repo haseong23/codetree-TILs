47def921640d0cc4e8e581a3412a6613a8b60405
haseong23 <151842154+haseong23@users.noreply.github.com>
2024-10-22 22:53:05 +0900

    Title: 모양대로 숫자 출력하기 2, Time: 140ms, Memory: 24MB, Status: Passed - CodeTree

--- 241022/모양대로 숫자 출력하기 2/print-out-numbers-in-specific-shape-2.py
+++ 241022/모양대로 숫자 출력하기 2/print-out-numbers-in-specific-shape-2.py
@@ -1,9 +1,10 @@
 n = int(input())
 num_arr = [2, 4, 6, 8]
-length = len(num_arr)
+cnt = 0
 
 for i in range(n):
     row = []
     for j in range(n):
-        row.append(num_arr[(i + j) % length])
+        row.append(num_arr[cnt % 4])
+        cnt += 1
     print(" ".join(map(str, row)))
